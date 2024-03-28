d557dbaad3448fa697c06982cb31b207538f0dd2
Tom Mossington <tmossington@gmail.com>
2024-03-28 16:22:13 -0400

    Added random_word function
    Changed previous random word function to be a daily change

--- randomanswer.py
+++ randomanswer.py
@@ -7,7 +7,7 @@ current_time = datetime.now()
 UNIX_reference = 1645678800
 
 # Function that picks a random word from the dictionary every day
-def random_answer(daily: bool = True):
+def daily_random_word(daily: bool = True):
     if daily == True:
         UNIX = datetime.now().timestamp()
         iteration = int(math.floor((UNIX_reference - UNIX) / 86400))
